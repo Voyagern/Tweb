db84ca31e1abc6b8e254c743f56ad72fd35d8aa0
Voyagern <ol.turevici@gmail.com>
2024-02-15 11:01:08 +0300

    my aplication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './download.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Turevici Oleg Student din rupa cr-222
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Voyagern"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My github
         </a>
       </header>
     </div>
